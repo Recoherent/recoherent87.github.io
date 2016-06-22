51c4068c5367b5d68ecb2889b063b027f0cf938c
McFluffins <aiden@sonication.net>
2016-06-22 15:07:25 -0500

    Added circle directory and all files involved
    For the webpage with the circle and the moving and stuff

---
+++ circle/circle_movement.js
@@ -0,0 +1,16 @@
+var canvas = document.getElementById("circle_box");
+var ctx = canvas.getContext("2d");
+var csize = 500;
+//grabbing the canvas, surface to draw on, and declaring size (square)
+
+ctx.fillStyle = "#000000";
+ctx.strokeRect (0, 0, 500, 500);
+//creating a visual representation of the canvas
+
+var circle = function(ms, x, y, r) {
+	ms.beginPath();
+	//starting the circle
+	//add start+end ms.arc(x, y, r, );
+	//creating circle path
+	
+}
