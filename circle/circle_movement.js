361071239d9e900682c81f9d03d25224452182d8
Recoherent <aiden@sonication.net>
2016-06-23 13:54:02 -0500

    Added a lot to circle_movement.js
    The circle draws, left and right movement (with arrow keys) functions
    properly, but not smoothly. Fix later?

--- circle/circle_movement.js
+++ circle/circle_movement.js
@@ -3,14 +3,40 @@ var ctx = canvas.getContext("2d");
 var csize = 500;
 //grabbing the canvas, surface to draw on, and declaring size (square)
 
+var cr = 30;
+var cx = 250;
+var cy = 250;
+//declaring
+
 ctx.fillStyle = "#000000";
 ctx.strokeRect (0, 0, 500, 500);
 //creating a visual representation of the canvas
 
 var circle = function(ms, x, y, r) {
+	ms.fillStyle = "#ffffff";
+	ms.fillRect (1, 1, 498, 498);
+	//refreshing screen
 	ms.beginPath();
 	//starting the circle
-	//add start+end ms.arc(x, y, r, );
+	ms.arc(x, y, r, 0, 2 * Math.PI);
 	//creating circle path
-	
-}
+	ms.stroke();
+	//drawing circle
+};
+
+circle(ctx, cx, cy, cr); //drawing circle at beginning
+
+document.addEventListener('keydown', function(event) {
+    if(event.keyCode == 37) {
+		
+        cx = cx - 3;
+		circle(ctx, cx, cy, cr);
+		
+    } //left arrow key
+    else if(event.keyCode == 39) {
+		
+        cx = cx + 3;
+		circle(ctx, cx, cy, cr);
+		
+    } //right arrow key
+});//movement of the circle
