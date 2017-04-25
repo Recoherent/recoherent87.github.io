7cf88f93983c3f13065366a498bd64f72b2f27a5
Recoherent <aiden@sonication.net>
2017-04-25 14:25:39 -0500

    more splash

--- main_page.js
+++ main_page.js
@@ -3,19 +3,19 @@ var max = 8
 var rand = Math.floor(Math.random() * (max - min +1)) + min
 switch (rand) {
   case 1:
-    document.getElementById("splash").innerHTML = "Made of electrons";
+    document.getElementById("splash").innerHTML = "Potentially useful";
     break;
   case 2:
     document.getElementById("splash").innerHTML = "Barely functional";
     break;
   case 3:
-    document.getElementById("splash").innerHTML = "Not useful at all";
+    document.getElementById("splash").innerHTML = "WARNING: May spontaneously create universes and/or cats";
     break;
   case 4:
     document.getElementById("splash").innerHTML = "Mildly enjoyable";
     break;
   case 5:
-    document.getElementById("splash").innerHTML = "I hope you enjoy gray!";
+    document.getElementById("splash").innerHTML = "Irregularly updated";
     break;
   case 6:
     document.getElementById("splash").innerHTML = "Distinctly monochrome";
