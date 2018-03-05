320724325d05159e8c864a18260eab5d03c0d8f9
Recoherent <aiden@sonication.net>
2018-03-05 14:40:04 -0600

    created clockbeta.js
    this makes the background range through the full range of RGB values as opposed to just 0-60

---
+++ clock/clockbeta.js
@@ -0,0 +1,30 @@
+var d, h, m, s, color;
+function displayTime() {
+    d = new Date();
+    h = d.getHours();
+    m = d.getMinutes();
+    s = d.getSeconds();
+
+    //add zero to the left of the numbers if they are single digits
+    if(h <= 9) h = '0'+h;
+    if(m <= 9) m = '0'+m;
+    if(s <= 9) s = '0'+s;
+
+    displaycolor = "#"+h+m+s;
+    
+    bh = (h*10).toString(16);
+    bm = (m*4).toString(16);
+    bs = (s*4).toString(16);
+    
+    backgroundcolor = "#"+bh+bm+bs;
+    
+    //set background color
+    document.body.style.background = backgroundcolor;
+    //set time
+    document.getElementById("hex").innerHTML = displaycolor;
+
+    //retrigger the function every second
+    setTimeout(displayTime, 1000);
+}
+
+displayTime();
