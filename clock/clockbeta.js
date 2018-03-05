f4b3d247923ae7f1ae34ec68e46407be9896e953
Recoherent <aiden@sonication.net>
2018-03-05 14:47:34 -0600

    rearranged varables

--- clock/clockbeta.js
+++ clock/clockbeta.js
@@ -4,19 +4,19 @@ function displayTime() {
     h = d.getHours();
     m = d.getMinutes();
     s = d.getSeconds();
-
-    //add zero to the left of the numbers if they are single digits
-    if(h <= 9) h = '0'+h;
-    if(m <= 9) m = '0'+m;
-    if(s <= 9) s = '0'+s;
-
-    displaycolor = "#"+h+m+s;
     
     bh = (h*10).toString(16);
     bm = (m*4).toString(16);
     bs = (s*4).toString(16);
     
     backgroundcolor = "#"+bh+bm+bs;
+    
+    //add zero to the left of the numbers if they are single digits
+    if(h <= 9) h = '0'+h;
+    if(m <= 9) m = '0'+m;
+    if(s <= 9) s = '0'+s;
+
+    displaycolor = "#"+h+m+s;
     
     //set background color
     document.body.style.background = backgroundcolor;
